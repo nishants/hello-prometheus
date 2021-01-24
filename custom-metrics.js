4e2e9dd2c2a2bb4d2c3834d3dadfea3f64344ed4
nishant <nishant.singh87@gmail.com>
2021-01-24 19:13:07 +0530

    Reset counter every 100 seconds.

--- custom-metrics.js
+++ custom-metrics.js
@@ -9,10 +9,10 @@ module.exports = (registry) => {
   // Counters can increase only
   const counter = new client.Counter({
     name: 'custom_increment',
-    help: 'Custom: This increments every seconds and then resets at 10 seconds',
+    help: 'Custom: This increments every seconds and then resets at 100 seconds',
   });
   setInterval(() => counter.inc(10), 1000);
-  setInterval(() => counter.reset(), 10000);
+  setInterval(() => counter.reset(), 100000);
 
   registry.registerMetric(counter);
 
